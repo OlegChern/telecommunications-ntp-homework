e3a81cfec54149531a0922ea3c99ec9337401809
OlegChern <OLChernyavsky@mail.ru>
2020-05-28 19:55:05 +0300

    Minor changes to CLI args

--- ntp.py
+++ ntp.py
@@ -3,10 +3,10 @@ import argparse
 from ntp_client import NtpClient
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("A simple NTP client which will print accurate time")
-    parser.add_argument("-a", "--address", help="Desired NTP server address", type=str, default="pool.ntp.org")
-    parser.add_argument("-p", "--port", help="Desired NTP server port", type=int, default=123)
-    parser.add_argument("-w", "--wait", help="Server response waiting time (in seconds)", type=int, default=123)
+    parser = argparse.ArgumentParser("a simple NTP client which will print accurate time")
+    parser.add_argument("-a", "--address", help="desired NTP server address", type=str, default="pool.ntp.org")
+    parser.add_argument("-p", "--port", help="desired NTP server port", type=int, default=123)
+    parser.add_argument("-w", "--wait", help="server response waiting time (in seconds)", type=int, default=123)
 
     args = parser.parse_args()
 
